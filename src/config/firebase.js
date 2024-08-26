f1d849364bf0285011e41ae3a8511490b55191a2
purush2525 <purushottamshukla460@gmail.com>
2024-08-26 16:14:23 +0530

    Contacts

---
+++ src/config/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCypWQi9bSA6PpBzMxQw8Hwgeqal2F6nLI",
+  authDomain: "vite-contact-19b93.firebaseapp.com",
+  projectId: "vite-contact-19b93",
+  storageBucket: "vite-contact-19b93.appspot.com",
+  messagingSenderId: "579510952948",
+  appId: "1:579510952948:web:ff6f69049d33dafbbb0fa1"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
